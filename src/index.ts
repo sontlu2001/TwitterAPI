435d9861d149a86a090140b2732bbe802e6892d0
DoSon <sonnvc2001@gmail.com>
2023-08-13 22:50:49 +0700

    connect mongo Atlas

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import usersRouter from './routes/users.routes'
+import databaseService from './services/database.service'
 
 const app = express()
 const port = 3000
+
 app.use(express.json())
 app.use('/users', usersRouter)
+//connect mongo Atlat
+databaseService.connect()
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/services/database.service.ts
@@ -0,0 +1,36 @@
+import { Collection, Db, MongoClient, ServerApiVersion } from 'mongodb'
+import { config } from 'dotenv'
+import User from '~/models/schemas/User.schema'
+
+config()
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4pi3zzt.mongodb.net/?retryWrites=true&w=majority`
+
+class DataBaseService {
+  private client: MongoClient
+  private db: Db
+
+  constructor() {
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    this.client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+      }
+    })
+    this.db = this.client.db(process.env.DB_NAME)
+  }
+
+  async connect() {
+    try {
+      // Send a ping to confirm a successful connection
+      await this.db.command({ ping: 1 })
+      console.log('Pinged your deployment. You successfully connected to MongoDB!')
+    }catch(error){
+      throw error
+      console.log(error);
+    }
+  }
+}
+const databaseService = new DataBaseService()
+export default databaseService
