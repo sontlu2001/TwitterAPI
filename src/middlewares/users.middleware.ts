7a578ebcbcb7a311c2941d7ebf45f771c0948f52
DoSon <sonnvc2001@gmail.com>
2023-08-17 16:15:30 +0700

    checkSchema để valide body register

--- src/middlewares/users.middleware.ts
+++ src/middlewares/users.middleware.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express'
+import { checkSchema } from 'express-validator'
 
 interface UserInfo {
   email: string
@@ -13,3 +14,39 @@ export const loginValidator = (req: Request, res: Response, next: NextFunction) 
   }
   next()
 }
+export const registerValidator = checkSchema({
+  name: {
+    notEmpty: true,
+    isLength: { options: { min: 3, max: 100 } },
+    trim: true,
+    errorMessage: 'Name must be between 3 and 100 characters'
+  },
+  email: {
+    notEmpty: true,
+    isEmail: true,
+    trim: true,
+    errorMessage: 'Invalid email'
+  },
+  password: {
+    notEmpty: true,
+    isString: true,
+    isStrongPassword: {
+      options: { minLength: 6, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 },
+      errorMessage:
+        'Password must be at least 6 characters long and contain at least 1 lowercase, 1 uppercase, 1 number and 1 symbol'
+    }
+  },
+  confirm_password: {
+    notEmpty: true,
+    custom: {
+      options: (value, { req }) => {
+        if (req.body.password !== value) throw new Error('Password confirmation does not match password')
+        return true
+      }
+    }
+  },
+  dateOfBirth: {
+    isISO8601: { options: { strict: true, strictSeparator: true } },
+    errorMessage: 'Invalid date of birth'
+  }
+})
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express'
 import { loginController, registerController } from '~/controllers/users.controllers'
-import { loginValidator } from '~/middlewares/users.middleware'
+import { loginValidator, registerValidator } from '~/middlewares/users.middleware'
+import { validate } from '~/utils/validation'
 const usersRouter = Router()
 
 usersRouter.post('/login', loginValidator, loginController)
-usersRouter.post('/register', registerController)
-
+/**
+ * Description. Register a new user
+ * Path: /users/register
+ * Method: POST
+ * Body: {name: string, email: string, password: string, confirm_password:string,dateOfBirth:string}
+ */
+usersRouter.post('/register', validate(registerValidator), registerController)
 
 export default usersRouter
