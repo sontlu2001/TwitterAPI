ef3812fb098fdb14399cf127fe3ddb03728e69a6
DoSon <sonnvc2001@gmail.com>
2023-08-26 15:58:19 +0700

    create google authentication url

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -3,6 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Link } from 'react-router-dom';
 
+const getGoogleAuthUrl = () => {
+    const { VITE_GOOGLE_CLIENT_ID, VITE_GOOGLE_REDIRECT_URI } = import.meta.env
+    const url = `https://accounts.google.com/o/oauth2/v2/auth`
+    const queryParams = {
+        client_id: VITE_GOOGLE_CLIENT_ID,
+        redirect_uri: VITE_GOOGLE_REDIRECT_URI,
+        response_type: 'code',
+        scope: [
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email'
+        ].join(' '),
+        prompt: 'consent',
+    }
+    const queryString = new URLSearchParams(queryParams).toString()
+    return `${url}?${queryString}`
+}
+const googleAuthUrl = getGoogleAuthUrl()
 const Home = () => {
     return (
         <>
@@ -16,7 +33,7 @@ const Home = () => {
             </div>
             <h1>Authentication Oauth2</h1>
 
-            <Link to='/login/oauth'>
+            <Link to={googleAuthUrl}>
                 Login with Google
             </Link>
         </>
